1daf03550d9ef770c117ee92938eee9d9c9f47c7
Ricardo Brito Brens <britobrensricardo@gmail.com>
2019-03-14 22:44:50 -0400

    Complete DayOne

---
+++ DayOne/DayOne/DayOne/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DayOne
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnShowMessage_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(entUserInput.Text))
+            {
+                btnShowMessage.Text = $"Hello {entUserInput}, welcome to 10 Days of Xamarin";
+            }
+            else
+            {
+                DisplayAlert("Error", "Your name can't be empty", "Oh right");
+            }
+        }
+    }
+}
